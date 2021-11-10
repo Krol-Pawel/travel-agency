b60c91605beb67ce778b0f71fbf9879e732ca718
Paweł Król <krol.pawel89@gmail.com>
2021-11-10 13:03:14 +0100

    add files and instal npm-run-all

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,20 +4,58 @@ import {Row, Col, Grid} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+import PropTypes from 'prop-types';
 
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
 
-const OrderForm = ({ tripCost, options, setOrderOption, currentValue }) =>(
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
+const OrderForm = ({ tripCost, options, setOrderOption }) =>(
   <Grid>
     <Row>
       {pricing.map((price) =>
-      <Col md={4} key={price.id}>
-        <OrderOption {...price} currentValue={options[price.id]} setOrderOption={setOrderOption}/>
-      </Col>)}
+        <Col md={4} key={price.id}>
+          <OrderOption {...price} currentValue={options[price.id]} setOrderOption={setOrderOption}/>
+        </Col>)}
       <Col xs={12}>
         <OrderSummary tripCost={tripCost} tripOptions={options}/>
       </Col>
+      <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
     </Row>
   </Grid>
 );
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+  setOrderOption: PropTypes.func,
+};
 
 export default OrderForm;
